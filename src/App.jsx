f7914f74dda5180dbc101ca0d9baac6e07e9c130
Abhishek Anand <anandabhishek3607@gmail.com>
2023-10-21 14:03:02 +0530

    woking with objects in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import Counter from "./Counter";
 import Dumbo from "./Dumbo";
+import ScoreKeeper from "./ScoreKeeper";
 
 function App() {
   return (
     <>
-      <h1>State Demo</h1>
+      <ScoreKeeper />
+      {/* <h1>State Demo</h1> */}
       {/* <Counter /> */}
-      <Dumbo />
+      {/* <Dumbo /> */}
     </>
   );
 }
