35998883638ae36e61a1cd343735e7f8b795e959
Abhishek Anand <anandabhishek3607@gmail.com>
2023-10-26 15:28:22 +0530

    passing states as props

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import ScoreKeeper from "./ScoreKeeper";
 import EmojiClicker from "./EmojiClicker";
 import ScoreKeepers from "./ScoreKeepers";
 import Lucky7 from "./Lucky7";
+import LuckyN from "./LuckyN";
 function App() {
   return (
     <>
       {/* <ScoreKeepers numPlayers={7} target={7} /> */}
-      <Lucky7 />
+      <LuckyN />
       {/* <EmojiClicker /> */}
       {/* <ScoreKeeper /> */}
       {/* <h1>State Demo</h1> */}
